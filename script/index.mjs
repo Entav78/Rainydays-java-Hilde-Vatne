19e059ed55c62766671b19c47c8809be46a9b41b
Entav78 <152163293+Entav78@users.noreply.github.com>
2024-05-04 17:00:00 +0200

    sha

--- script/index.mjs
+++ script/index.mjs
@@ -2,8 +2,7 @@
 
 import { API_URL } from "./constants.mjs";
 import { fetchData } from "./fetchData.mjs";
-//import { generateJacketsHtml } from "./generateJacketsHtml.mjs";
-import { setupCartLink } from "./cart/cartLink.mjs";  // Ensure this function is correctly named and exported in 'cartLink.mjs'
+import { setupCartLink } from "./cart/cartLink.mjs";  
 import { updateCartCount } from './cart/updateCartCount.mjs';
 import { handleJacketsFiltering } from "./handleJacketsFiltering.mjs";
 
@@ -11,7 +10,7 @@ import { handleJacketsFiltering } from "./handleJacketsFiltering.mjs";
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    setupCartLink();  // Set up the cart link handling
+    setupCartLink();  
     updateCartCount();
     
     async function fetchAndRenderJacketsHtml() {
@@ -20,27 +19,17 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Loader element not found');
             return;
         }
-
         try {
-            // Show the loader
             loader.style.display = 'block';
-
-            // Fetch the data from the API and store it as 'jackets'
             const jackets = await fetchData(API_URL);
-
-            // Generate HTML for all the jackets
             handleJacketsFiltering(jackets);
-
         } catch (error) {
             console.error("Failed to process jackets:", error);
         } finally {
-            // Hide the loader regardless of success or failure
             loader.style.display = 'none';
         }
     }
-
-    fetchAndRenderJacketsHtml();
-    
+    fetchAndRenderJacketsHtml();    
 });
 
 
