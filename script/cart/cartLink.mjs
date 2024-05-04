4dbd6d6a06d4fd4857cfc8b7d23d0b3434378378
Entav78 <152163293+Entav78@users.noreply.github.com>
2024-05-04 19:15:43 +0200

    cartlink

--- script/cart/cartLink.mjs
+++ script/cart/cartLink.mjs
@@ -12,3 +12,13 @@ export function setupCartLink() {
       });
   }
 }
+/*
+document.getElementById('cartLink').addEventListener('click', () => {
+    const cart = JSON.parse(localStorage.getItem("cart") || '[]');
+    if (cart.length === 0) {
+      alert("Your cart is currently empty. Please add some items before heading to the cart.");
+    } else {
+      window.location.href = 'https://entav78.github.io/Rainydays-java-Hilde-Vatne/html/cart/index';
+    }
+  });
+  */
