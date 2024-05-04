dc6f6c3a714c78c92f7d08a4286af074d67a4c94
Entav78 <152163293+Entav78@users.noreply.github.com>
2024-05-04 18:02:19 +0200

    right path for confirm checkout

--- script/cart/completePurchaseButton.mjs
+++ script/cart/completePurchaseButton.mjs
@@ -3,7 +3,7 @@ export function confirmOrderButton() {
   confirmOrderBtn.textContent = "Confirm your Order";
 
   confirmOrderBtn.addEventListener("click", () => {
-    window.location.href = 'http://127.0.0.1:5500/html/newCheckout/newConfirmation/index';
+    window.location.href = 'https://entav78.github.io/Rainydays-java-Hilde-Vatne/html/newCheckout/newConfermation/index';
   });
 
   return confirmOrderBtn;
