236fa232a6ac0751aeb51ae00c8756f4d6be3bdc
Entav78 <152163293+Entav78@users.noreply.github.com>
2024-05-04 19:26:30 +0200

    bug link?

--- script/cart/cartLink.mjs
+++ script/cart/cartLink.mjs
@@ -7,18 +7,8 @@ export function setupCartLink() {
           if (cart.length === 0) {
               alert("Your cart is empty.");
           } else {
-              window.location.href = 'http://127.0.0.1:5500/html/cart/index.html';
+              window.location.href = 'https://entav78.github.io/Rainydays-java-Hilde-Vatne/html/cart/index';
           }
       });
   }
 }
-/*
-document.getElementById('cartLink').addEventListener('click', () => {
-    const cart = JSON.parse(localStorage.getItem("cart") || '[]');
-    if (cart.length === 0) {
-      alert("Your cart is currently empty. Please add some items before heading to the cart.");
-    } else {
-      window.location.href = 'https://entav78.github.io/Rainydays-java-Hilde-Vatne/html/cart/index';
-    }
-  });
-  */
